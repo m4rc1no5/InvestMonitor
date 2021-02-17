1b89aff3a3686feb61165f8846017c959e5b8238
Marcin Zaremba <marcin.zaremba@bm.pl>
2021-02-18 00:39:12 +0100

    #6 - GPW - ETF S&P500

--- src/main/java/pl/marceen/investmonitor/gpw/entity/Instrument.java
+++ src/main/java/pl/marceen/investmonitor/gpw/entity/Instrument.java
@@ -4,7 +4,9 @@ package pl.marceen.investmonitor.gpw.entity;
  * @author Marcin Zaremba
  */
 public enum Instrument {
-    ETFSP500("LU0496786574", "ETF S&P500");
+    ETFSP500("LU0496786574", "ETF S&P500"),
+    ETFDAX("LU0252633754", "ETF DAX"),
+    BETAM40TR("PLBETF400025", "ETF MWIG40TR");
 
     private final String id;
     private final String fullName;
